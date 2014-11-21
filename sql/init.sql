2ec3da102916301ffd99c194c1d0c6ccf45f9c49
petr_bu <petr_bu@PC00151.volga.magnat.lan>
2014-11-21 18:33:34 +0400

    init sql script added

---
+++ sql/init.sql
@@ -0,0 +1,40 @@
+drop schema sm_navigator if exists;
+create schema sm_navigator;
+
+create table sm_navigator.store (
+	id integer primary key,
+	name varchar (255),
+	customer varchar (255),
+	address varchar (255),
+	tel varchar (255),
+	channel varchar (255),
+	coverage_type varchar (255),
+	golden_status varchar (255),
+	psr integer,
+	visit_frequency varchar (255)
+);
+create table sm_navigator.store_stat (
+	id integer primary key,
+	store integer,
+	last_visit date,
+	next_visit date,
+	turnover_previous_month float,
+	turnover_current_month float,
+	total_distribution float,
+	golden_distribution float
+);
+create table sm_navigator.psr (
+	id integer primary key,
+	name varchar (255),
+	project varchar (255),
+	tel varchar (255),
+	branch varchar (255),
+	department varchar (255)
+);
+create table sm_navigator.psr_route (
+	id integer primary key,
+	visit_date date,
+	psr integer,
+	store integer
+);
+create table sm_navigator.sync_info ();
