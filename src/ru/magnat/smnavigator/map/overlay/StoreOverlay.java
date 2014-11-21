1e4ddd47524965c77766f2f6a08253ccef626dfa
petr_bu <petr_bu@PC00151.volga.magnat.lan>
2014-11-21 19:04:44 +0400

    other

--- src/ru/magnat/smnavigator/map/overlay/StoreOverlay.java
+++ src/ru/magnat/smnavigator/map/overlay/StoreOverlay.java
@@ -1,25 +1,34 @@
 package ru.magnat.smnavigator.map.overlay;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import android.content.Context;
 import android.graphics.drawable.Drawable;
-import android.util.Log;
 import android.widget.Toast;
 
-import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.MapView;
 import com.google.android.maps.OverlayItem;
+import com.readystatesoftware.mapviewballoons.BalloonItemizedOverlay;
 
-public class StoreOverlay extends ItemizedOverlay<OverlayItem> {
+public class StoreOverlay extends BalloonItemizedOverlay<OverlayItem> {
 
-	private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
+	private List<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
+	private Context mContext;
+	
+	public StoreOverlay(Drawable defaultMarker, MapView mapView) {
+		super(boundCenter(defaultMarker), mapView);
+		mContext = mapView.getContext();
+	}
 
-	public StoreOverlay(Drawable defaultMarker) {
-		super(boundCenterBottom(defaultMarker));
+	public void addOverlay(OverlayItem overlay) {
+	    mOverlays.add(overlay);
+	    populate();
 	}
 
 	@Override
-	protected OverlayItem createItem(int arg0) {
-		return mOverlays.get(arg0);
+	protected OverlayItem createItem(int i) {
+		return mOverlays.get(i);
 	}
 
 	@Override
@@ -27,16 +36,10 @@ public class StoreOverlay extends ItemizedOverlay<OverlayItem> {
 		return mOverlays.size();
 	}
 
-	public void addOverlay(OverlayItem overlay) {
-		mOverlays.add(overlay);
-		populate();
-	}
-	
 	@Override
-	protected boolean onTap(int index) {
-		Log.d("", "title = " + mOverlays.get(index).getTitle());
-				
-		return super.onTap(index);
+	protected boolean onBalloonTap(int index, OverlayItem item) {
+		Toast.makeText(mContext, "onBalloonTap for overlay index " + index, Toast.LENGTH_LONG).show();
+		return true;
 	}
 	
 }
